a256d12a0971b1107d78839012726491ca877704
Romar Macaraeg <romarmph@gmail.com>
2024-06-01 18:51:14 +0800

    Linux configs

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+
+source ~/.config/fish/functions/custom_abbr.fish
+
+set fish_greeting
+
+zoxide init fish | source
+starship init fish | source
---
+++ fish/functions/custom_abbr.fish
@@ -0,0 +1,3 @@
+abbr el 'eza -lT -L=1 --icons --color --hyperlink'
+abbr ea 'eza -lT -L=1 --icons --color --hyperlink -a'
+abbr e2 'eza -lT -L=2 --icons --color --hyperlink -a'
